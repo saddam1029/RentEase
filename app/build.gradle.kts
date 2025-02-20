8fd8adb177785421ab2326f6c7569b2eff7ed56e
Syed Saddam <saddam.jaff07@gmail.com>
2025-02-20 18:09:26 +0500

    testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.rentease"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.rentease"
-        minSdk = 31
-        targetSdk = 34
+        minSdk = 28
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
